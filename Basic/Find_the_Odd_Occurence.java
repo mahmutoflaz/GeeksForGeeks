d15e5f1b2fa51f82e34b5398128de2a29985ef50
Mahmut Oflaz <mahmutoflaz@users.noreply.github.com>
2017-05-24 22:04:59 +0300

    Find the Odd Occurence

---
+++ Basic/Find_the_Odd_Occurence.java
@@ -0,0 +1,25 @@
+/*
+ * http://practice.geeksforgeeks.org/problems/find-the-odd-occurence/0
+ */
+ 
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numOfCases = sc.nextInt();
+        for(int i = 0; i< numOfCases; i++){
+            sc.nextInt();
+            sc.nextLine();
+            String p = sc.nextLine();
+            System.out.println(findOdd(Arrays.stream(p.split(" ")).map(Integer::parseInt).toArray(Integer[]::new)));
+        }
+    }
+
+    public static Integer findOdd(Integer[] arr){
+        int res = 0;
+        for(Integer i : arr) res = res ^ i;
+        return res;
+    }
+}
